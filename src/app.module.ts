1c30cd953e92ffecd26ec68869b62b5d0a781008
IhorVenher <73756717+IhorWenher@users.noreply.github.com>
2021-12-26 20:32:56 +0200

    added_env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     AccountsModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://IhorVenher:kbYqO1r8UKSi26qy@cluster0.0lamy.mongodb.net/accounts_db?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(process.env.DB_HOST),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-/* kbYqO1r8UKSi26qy */
